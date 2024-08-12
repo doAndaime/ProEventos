6c459b4ce3d593a055099266219a6404476e8ddd
Alberto Silva <alberto.silva@inovflow.pt>
2024-08-12 22:27:44 +0100

    Seção 2: DotNet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[] {
+            new Evento() {
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Amares",
+                Lote = "1 Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImgURL = "foto.png"
+            },
+            new Evento() {
+                EventoId = 2,
+                Tema = "Angular 11 e .NET 5 Yoh",
+                Local = "Caires",
+                Lote = "2 Lote",
+                QtdPessoas = 500,
+                DataEvento = DateTime.Now.AddDays(3).ToString(),
+                ImgURL = "foto.png"
+            }
+        };
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(e => e.EventoId == id);
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo de Delete com id = {id}";
+        }
+    }
+}
